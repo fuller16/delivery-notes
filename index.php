da9a556cc17e03e00f3553cd8b8e93f4446ed4e9
Mob Dev <mobidev86@gmail.com>
2024-10-03 19:26:52 +0530

    changes

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php if(isset($_GET['key'])){
 		$key = $_GET['key'];
 		$decrypted = base64_decode($key);
-		$replacestring = str_replace("/var/www/html/staging-temp/","/home/pinpointdev/Dropbox/onpoint.pinpoint/",$decrypted);
+	//	$replacestring = str_replace("/var/www/html/staging-temp/","/home/pinpointdev/Dropbox/onpoint.pinpoint/",$decrypted);
+		$replacestring = '/var/www/html/staging-temp/'.$decrypted;
 		echo file_get_contents($replacestring);
 }else{
 	echo "Delivery Note Not available";
