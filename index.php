30855a33081c1c9844db8b99ecf265898b4b0ef6
Mob Dev <mobidev86@gmail.com>
2024-10-04 12:23:24 +0530

    delivery notes issue fixed

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 		$key = $_GET['key'];
 		$decrypted = base64_decode($key);
 	//	$replacestring = str_replace("/var/www/html/staging-temp/","/home/pinpointdev/Dropbox/onpoint.pinpoint/",$decrypted);
-		$replacestring = '/var/www/html/staging-temp/'.$decrypted;
+		$replacestring = $decrypted;
 		echo file_get_contents($replacestring);
 }else{
 	echo "Delivery Note Not available";
